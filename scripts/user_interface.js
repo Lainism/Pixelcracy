3fc17096495c1826876f5f7dbeea4635980fa06a
Lauri Tervonen <lauri.tervonen@aalto.fi>
2014-07-15 21:30:44 +0300

    dummy module interaction

--- scripts/user_interface.js
+++ scripts/user_interface.js
@@ -1,2 +1,6 @@
 console.log("user interface module loaded");
 
+define(function (require) {
+	var user_state = require('./user_state');
+	console.log(user_state.get_active_tool());
+});
--- scripts/user_state.js
+++ scripts/user_state.js
@@ -1,1 +1,9 @@
 console.log("user state module loaded");
+
+define(function () {
+	return {
+            get_active_tool: function() {
+                return "Pencil";
+            }
+        };
+});
